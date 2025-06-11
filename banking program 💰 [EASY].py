e162015a3dea43ac817869f52f1d20032b8bba82
TANVI PAWAR <tanvipawar977@gmail.com>
2025-06-11 20:05:01 +0530

    uploaded a python file 1

---
+++ banking program 💰 [EASY].py
@@ -0,0 +1,72 @@
+# Python banking program 💰[EASY]
+
+def show_balance(balance):
+    print("***************")
+    print(f"yor balance is ${balance:.2f}")
+    print("***************")
+
+def deposit():
+    print("***************")
+    amount = float(input("Enter an amount to be deposited: "))
+    print("***************")
+    
+    if amount < 0:
+        print("***************")
+        print("That's not a valid amount")
+        print("***************")
+        return 0 
+    else: 
+        return amount    
+
+def withdraw(balance):
+    print("***************")
+    amount = float(input("Enter amount to be withdrawn: "))
+    print("***************")
+    000
+    
+    
+    if amount > balance:
+        print("insufficient funds")
+        return 0 
+    elif amount < 0:
+        print("Amount must be greater than 0")
+        return 0 
+    else:
+        return amount        
+
+
+def main():
+    balance = 0
+    is_running = True
+
+    while is_running ==True:
+        print("***************")
+        print("Banking Program")
+        print("***************")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+        print("***************")
+        
+        choice = input("Enter your choice (1-4): ")
+        
+        if choice == '1':
+            show_balance(balance)
+        elif choice == '2':
+            balance += deposit()
+        elif choice == '3':
+            balance -= withdraw(balance)
+        elif choice == '4':
+            is_running = False 
+        else:
+            print("***************")
+            print("That is not a valid choice") 
+            print("***************")          
+     
+    print("***************")            
+    print("Thank you! have a nice day!")
+    print("***************")
+
+if __name__== "__main__":
+    main()
